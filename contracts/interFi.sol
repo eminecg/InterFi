979cec7537a56f3d82fd85fb2fdbb1714ea4f5d2
Emine Çığ <58005344+eminecg@users.noreply.github.com>
2022-08-11 16:04:07 +0300

    small fix on structs

--- contracts/interFi.sol
+++ contracts/interFi.sol
@@ -1,5 +1,5 @@
 // SPDX-License-Identifier: UNLICENSED
-pragma solidity ^0.8.8;
+pragma solidity ^0.8.9;
 
 // Import this file to use console.log
 import "hardhat/console.sol";
@@ -7,38 +7,36 @@ import "hardhat/console.sol";
 error Already_has_two_parent_error();
 
 contract User {
+
     enum Role {
         PARENT,
         CHILD
     }
 
-    struct PersonalData {
+    struct   PersonalData  {
         uint id;
         string firstName;
         string lastName;
         Role role;
-    }
+        address walletAddress;
+    } 
+
+   
+    
 
     mapping(address => uint256) public balances;
 
+    function createUser() public {}
+
+    function deleteUser() public {}
+
     function transactionHistory() public {}
 
-    function getBalance(address index) public view returns (uint256) {
-        return balances[index];
-    }
+    function getBalance() public {}
 
-    function ageCalc(uint256 birthday, uint256 today) public returns (uint256) {
-        uint startDate = birthday; // 2012-12-01 10:00:00
-        uint endDate = today; // 2012-12-07 10:00:00
+    function ageCalc() public {}
 
-        uint daysDiff = (endDate - startDate) / 60 / 60 / 24; // 6 days
-
-        return daysDiff;
-    }
-
-    function fund() public payable {
-        balances[msg.sender] += msg.value / 10**18;
-    }
+    function fund() public payable {}
 
     function withdraw() public {}
 
@@ -55,15 +53,46 @@ contract Parent is User {
     uint256 childCount = 0;
     PersonalData[] public parents;
 
+     struct ParentData{
+         PersonalData personalData;
+         uint _id;
+         // ..
+     }
+    struct ChildData{
+         PersonalData personalData;
+         uint _id;
+         // ..
+     }
+
+    
+
     function addParent(
         uint id,
         string memory _firstName,
         string memory _lastName,
-        Role role
+        Role role,
+        address walletAddress
     ) public {
         if (parents.length > 2) {
             revert Already_has_two_parent_error();
         }
-        parents.push(PersonalData(id, _firstName, _lastName, role));
+        parents.push(
+            PersonalData(id, _firstName, _lastName, role, walletAddress)
+        );
     }
+
+   
+}
+
+contract  Child is User {
+
+  
+   struct ChildData{
+         PersonalData personalData;
+         uint _id;
+         // ..
+     }   
+
+    
+
 }
