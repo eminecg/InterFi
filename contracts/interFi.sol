44d31b07bd74c755aabe34e183ae42de1731b346
Hasan Yiğit Kaya <hasanyigit.kaya@bahcesehir.edu.tr>
2022-08-11 11:34:18 +0300

    basic imp.

---
+++ contracts/interFi.sol
@@ -0,0 +1,66 @@
+// SPDX-License-Identifier: UNLICENSED
+pragma solidity ^0.8.9;
+
+// Import this file to use console.log
+import "hardhat/console.sol";
+
+error Already_has_two_parent_error();
+
+contract User {
+    enum Role {
+        PARENT,
+        CHILD
+    }
+
+    struct PersonalData {
+        uint id;
+        string firstName;
+        string lastName;
+        Role role;
+        address walletAddress;
+    }
+
+    mapping(address => uint256) public balances;
+
+    function createUser() public {}
+
+    function deleteUser() public {}
+
+    function transactionHistory() public {}
+
+    function getBalance() public {}
+
+    function ageCalc() public {}
+
+    function fund() public payable {}
+
+    function withdraw() public {}
+
+    receive() external payable {
+        fund();
+    }
+
+    fallback() external payable {
+        fund();
+    }
+}
+
+contract Parent is User {
+    uint256 childCount = 0;
+    PersonalData[] public parents;
+
+    function addParent(
+        uint id,
+        string memory _firstName,
+        string memory _lastName,
+        Role role,
+        address walletAddress
+    ) public {
+        if (parents.length > 2) {
+            revert Already_has_two_parent_error();
+        }
+        parents.push(
+            PersonalData(id, _firstName, _lastName, role, walletAddress)
+        );
+    }
+}
