b25ca5fc5e592ee0f331528e0001fa04f65e647a
Emine Çığ <58005344+eminecg@users.noreply.github.com>
2022-08-12 16:10:19 +0300

    add addChild, addParent

--- contracts/interFi.sol
+++ contracts/interFi.sol
@@ -4,103 +4,104 @@ pragma solidity ^0.8.9;
 // Import this file to use console.log
 import "hardhat/console.sol";
 
-error Already_has_two_parent_error();
 
-contract User {
+error InterFi__NotOwner();
+error Child__isUnderage();
+error Child__Cant_Have_Child_Without_Parents();
+error Child__Parent_Not_Found_Add_Parent_First();
 
-    enum Role {
-        PARENT,
-        CHILD
+contract Interfi{
+
+    address public owner;
+    bool isUnderage = true;
+
+    struct Parent {
+        address parentAddress;
+        address[]  children;
+        string name;
     }
 
-    struct   PersonalData  {
-        uint id;
-        string firstName;
-        string lastName;
-        Role role;
-        address walletAddress;
-    } 
+    struct Child {
+        address childAddress;
+        uint birthday;
+        address[] parents;
+        string name;
+    }
+
+    uint parentCount= 0;
+    uint childCount= 0;
+
+    mapping(address => Child) public addressToChild;
+    mapping(address => Parent) public addressToParent;
 
    
-    
-
+    mapping(address => address[]) public parentAddressToChilds;
     mapping(address => uint256) public balances;
 
-    function createUser() public {}
-
-    function deleteUser() public {}
-
-    function transactionHistory() public {}
-
-    function getBalance() public {}
-
-
-    function ageCalc(uint256 birthday, uint256 today)
-        public
-        pure
-        returns (uint256)
-    {
-        uint startDate = birthday; // 2012-12-01 10:00:00
-        uint endDate = today; // 2012-12-07 10:00:00
-}
-
-    function fund() public payable {}
-
-    function withdraw() public {}
-
-    receive() external payable {
-        fund();
+    modifier onlyOwner (){
+    if (msg.sender != owner)
+        {revert InterFi__NotOwner();}
+        _;
     }
 
-    fallback() external payable {
-        fund();
-    }
-}
+    uint counter = 0;
 
-contract Parent is User {
-    uint256 childCount = 0;
-    PersonalData[] public parents;
 
-     struct ParentData{
-         PersonalData personalData;
-         uint _id;
-         // ..
-     }
-    struct ChildData{
-         PersonalData personalData;
-         uint _id;
-         // ..
-     }
+       address[]  childrens ;
+      function addParent(address payable _parent, string memory _name) public {  
+           
+        
 
-    
+            addressToParent[_parent] = Parent({parentAddress:_parent,children:childrens,name:_name});
+            
+        } 
 
-    function addParent(
-        uint id,
-        string memory _firstName,
-        string memory _lastName,
-        Role role,
-        address walletAddress
-    ) public {
-        if (parents.length > 2) {
-            revert Already_has_two_parent_error();
+        address[]  parents ;
+        function addChild(address payable _parent,address payable _child, uint256 _birthday ,address payable _otherParent, string memory _name) public {  
+           
+         parents.push(_parent);
+         parents.push(_otherParent);
+
+            addressToChild[_child] = Child({childAddress:_child,birthday:_birthday,parents:parents,name:_name});
+            addressToParent[_parent].children.push(_child);
+
         }
-        parents.push(
-            PersonalData(id, _firstName, _lastName, role, walletAddress)
-        );
+
+        function getBalance() public view returns (uint256) {
+            return address(this).balance;
+        }
+    
+        function ageCalc(uint256 birthdayInSec) public returns (uint256) {
+            uint256 ageInSec = (block.timestamp - birthdayInSec);
+            if ((ageInSec / 31536000) >= 18) {
+                isUnderage = false;
+            } else {
+                isUnderage = true;
+            }
+            return (ageInSec / 31536000); // year in seconds
+        }
+    
+    
+        function getChild(uint256 index) public view returns (address) {
+        }
+    
+        function fund() public payable {
+        }
+    
+        // burası çalışmıyor ??
+        function withdraw() public payable {
+            if (isUnderage) {
+                revert Child__isUnderage();
+            } else {}
+        }
+    
+        receive() external payable {
+            fund();
+        }
+    
+        fallback() external payable {
+            fund();
+        }
     }
 
-   
-}
-
-contract  Child is User {
-
-  
-   struct ChildData{
-         PersonalData personalData;
-         uint _id;
-         // ..
-     }   
-
     
-
-}
