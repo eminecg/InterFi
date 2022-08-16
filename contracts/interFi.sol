1fe9d6d72728debb301bc4437f0fa316217fcdfd
Emine Çığ <58005344+eminecg@users.noreply.github.com>
2022-08-16 10:21:43 +0300

    fix

--- contracts/interFi.sol
+++ contracts/interFi.sol
@@ -5,15 +5,15 @@ pragma solidity ^0.8.9;
 import "hardhat/console.sol";
 
 //0x0000000000000000000000000000000000000000
-//0x302955b74C969aA09bb270DAa775B65Fc9b7Bc29
-// 0x302955b74C969aA09bb270DAa775B65Fc9b7Bc29, 1723720445 ,Yiğit
+//0x5B38Da6a701c568545dCfcB03FcB875f56beddC4
+//0xAb8483F64d9C6d1EcF9b849Ae677dD3315835cb2, 1723720445 ,Yiğit
+//0xAb8483F64d9C6d1EcF9b849Ae677dD3315835cb2
 error This_Parent_Already_Exist();
 error InterFi__NotOwner();
 error Child__isUnderage();
 error Child__Cant_Have_Child_Without_Parents();
 error Child__Parent_Not_Found_Add_Parent_First();
 error There_is_no_child_belongs_parent();
-
 contract Interfi {
     address private owner;
 
@@ -94,22 +94,10 @@ contract Interfi {
         return addressToParent[msg.sender].children;
     }
 
-    function getBalance() public view returns (uint256) {
-        return address(this).balance;
-    }
-
-    function getChildren() public view returns (address[] memory) {
-        return addressToParent[msg.sender].children;
-    }
-
-    event Purchase(
-        address indexed _invester,
-        uint256 _amount
-    );
+    
+    // get amount of ether from child balance , send to the msg.sender wallet
     function fund(address payable _child) public payable {
-                 
-
-        // check the parent exist
+         // check the parent exist
         Parent storage parent = addressToParent[msg.sender];
         require(parent.Address != address(0), "There_Is_No_Such_Parent()");
         
@@ -134,11 +122,52 @@ contract Interfi {
 
     }
 
-
-
-    function withdraw() public payable {}
-
     receive() external payable {}
 
     fallback() external payable {}
+
+    event Purchase(
+        address indexed _invester,
+        uint256 _amount
+    );
+    
+    // parent can get amount of coin from his/her child balance ,msg.sender has to be parent
+    function withdrawParent(address payable _child) public payable {
+                 
+
+        // check the parent exist
+        Parent storage parent = addressToParent[msg.sender];
+        require(parent.Address != address(0), "There_Is_No_Such_Parent()");
+        
+        // check this child belongs to this parent 
+        uint size =parent.children.length;
+        uint index;
+        for (uint i = 0; i < size; i++) {
+            if(parent.children[i]==_child){
+                index=i;            
+            }
+        }
+        if(index>=0){
+            
+            Child storage child = addressToChild[_child];                        
+            emit Purchase(msg.sender, 1);
+            child.amount-=msg.value;
+            parent.Address.transfer(msg.value); // send the amount of value to the parent address 
+            
+        }
+        else{
+            
+            revert There_is_no_child_belongs_parent();
+        }
+
+    }
+
+    //  child can get amount of coin from his/her balance, msg.sender has to be child
+     function withdrawChild(address payable _child) public payable {
+         
+         
+
+     }
+
+
 }
