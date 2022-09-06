9f1f7adfe7eed6ebadff7eb228b858d11618dc2f
Hasan Yiğit Kaya <hasanyigit.kaya@bahcesehir.edu.tr>
2022-09-06 12:57:14 +0300

    ss

--- scripts/03-upgrade-contract.js
+++ scripts/03-upgrade-contract.js
@@ -3,7 +3,7 @@ const { ethers, upgrades } = require('hardhat');
 async function main() {
     const InterFiV2 = await ethers.getContractFactory('InterFiV2');
     console.log('Upgrading InterFi...');
-    await upgrades.upgradeProxy('0xD2e4BF24e77A02Ad60C873D7CD42f921CC212750', InterFiV2);
+    await upgrades.upgradeProxy('0x78FBaAaa1065D46444843d642c7Fdd8ec74D5eF1', InterFiV2);
     console.log('InterFi upgraded');
 }
 
